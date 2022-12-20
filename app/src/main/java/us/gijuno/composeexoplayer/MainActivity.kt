a06c211016dc9a7f3e6bec43535b461270a49b84
Gijuno <46197097+Gijuno@users.noreply.github.com>
2022-12-20 13:44:29 +0900

    set center

--- app/src/main/java/us/gijuno/composeexoplayer/MainActivity.kt
+++ app/src/main/java/us/gijuno/composeexoplayer/MainActivity.kt
@@ -1,36 +1,27 @@
 package us.gijuno.composeexoplayer
 
 import android.os.Bundle
-import android.view.View
-import android.widget.ScrollView
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.ui.StyledPlayerView
-import us.gijuno.composeexoplayer.ui.theme.ComposeExoplayerTheme
-import kotlin.coroutines.coroutineContext
 
 class MainActivity : ComponentActivity() {
     private val videoUris = arrayListOf(
@@ -49,41 +40,30 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val listState = rememberLazyListState()
             val scrollState = rememberScrollState()
-            // 1
-//            LazyColumn(
-//                modifier = Modifier.fillMaxSize(),
-//            ) {
-//                item {
-//                    VideoView(videoUris[0])
-//                }
-//            }
-            // 2
-//            VideoView(uri = videoUris[0])
-
-            // 3
-            Row {
-                Column(
-                    modifier = Modifier.fillMaxHeight()
+            
+            Row(
+                Modifier
+                    .horizontalScroll(scrollState)
+                    .fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(end = 50.dp),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    for (i in 0 until 8) {
-                        Box(Modifier.size(90.dp)) {
-                            VideoView(uri = videoUris[i])
-                        }
-                    }
-                }
-                Column(
-                    modifier = Modifier.fillMaxHeight()
-                ) {
-                    for (i in 8 until videoUris.size) {
-                        Box(Modifier.size(90.dp)) {
+                    for (i in 0 until videoUris.size) {
+                        Log.d("delay", i.toString())
+                        Box(Modifier
+                            .size(200.dp)
+                            .padding(10.dp)) {
                             VideoView(uri = videoUris[i])
                         }
                     }
                 }
             }
-
         }
     }
 }
